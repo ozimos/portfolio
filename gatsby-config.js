ef4062f184b9a19b4495cef490be84cf69490b62
Tovieye Moses Ozi <tovieye.ozi@gmail.com>
2020-03-03 21:02:26 +0100

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,14 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+      
     },
+    {
+      resolve: "gatsby-theme-portfolio",
+      options: {
+        contentPath: `src/content` // the path to your markdown files
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
